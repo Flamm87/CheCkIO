9455a735f53483ff691c849c473955dcf0cabf63
Flamm <Flamm@ADMIN203>
2019-03-25 14:33:51 +0200

    Finish Version

--- create_paths.py
+++ create_paths.py
@@ -1,56 +1,38 @@
 def get_layer(net, crush):
-    temp = []
-    for x in net.copy():
-        if crush in x:
-            temp.append("".join(x).replace(crush, ""))
-    return temp
+    """ Ету функцию можно заменить на
+    source_paths = lambda n, c: ["".join(x).replace(c, "") for x in n if c in x] в disconnected_users"""
+    return ["".join(x).replace(crush, "") for x in net if crush in x]
 
 
-def make_path(net, crush, path, old_path=0):
-    if path != 0 and old_path == path:  # Проверка на кольцо
-        return path
-    if len(path) == 0:  # Добавляем вершину
-        path.append(crush)
-    for node in path.copy():
-        layer = get_layer(net, node[-1])  # Для каждой нижней точки вибираем список следующих звеньев
-        old_path = path.copy()  # сохраняем старый список путей
-        for x in layer:  # Формируем новый
-            if x == path[0][0]:
-                continue
-            if len(node) > 1:
-                if node[-2] == x:
-                    continue
-            if node in path:
-                path.remove(node)
-            path.append(node + x)
-    return make_path(net.copy(), None, path, old_path)
+def make_path(links, sourse, crushes):
+    paths = list(sourse)
+    for path in paths.copy():
+        p = [path + i for i in links[path[-1]] if (i not in path) and (i not in crushes)]
+        if len(p) == 0:
+            continue
+        paths.remove(path)
+        paths.extend(p)
+    if paths == sourse:
+        return paths
+    return make_path(links, paths, crushes)
 
-
-def count_pigeons(users, source, paths, crushes):
-    points = set(crushes)
-    for path in paths:
-        if source not in path:
-            points |= set(path)
+def disconnected_users(net, users, source, crushes):
+    if source in crushes:
+        return sum(users.values())
+    link_nodes = {x: get_layer(net, x) for x in users.keys()}
+    source_paths = make_path(link_nodes, source, crushes)
+    set_con_point = set()
+    for x in source_paths:
+        set_con_point |= set(x)
+    broken_points = set(users.keys()) - set_con_point
     count = 0
-    for x in list(points):
+    for x in list(broken_points):
         count += users[x]
     return count
 
 
-def disconnected_users(net, users, source, crushes):
-    paths = []
-    if source in crushes:
-        return sum(users.values())
-    for crush in crushes:
-        paths.extend(make_path(net.copy(), crush, [], 0))
-    return count_pigeons(users, source, paths, crushes)
-
-
 if __name__ == '__main__':
-    disconnected_users([["A", "B"], ["A", "C"], ["A", "D"], ["A", "E"], ["A", "F"]],
-                       {"A": 10, "C": 10, "B": 10, "E": 10, "D": 10, "F": 10}, "A", ["B", "C"])
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert disconnected_users([
+    disconnected_users([
         ['A', 'B'],
         ['B', 'C'],
         ['C', 'D']
@@ -60,35 +42,10 @@ if __name__ == '__main__':
         'C': 30,
         'D': 40
     },
-        'A', ['A']) == 100, "First"
+        'A', ['C'])
 
-    assert disconnected_users([
-        ['A', 'B'],
-        ['B', 'D'],
-        ['A', 'C'],
-        ['C', 'D']
-    ], {
-        'A': 10,
-        'B': 0,
-        'C': 0,
-        'D': 40
-    },
-        'A', ['B']) == 0, "Second"
-
-    assert disconnected_users([
-        ['A', 'B'],
-        ['A', 'C'],
-        ['A', 'D'],
-        ['A', 'E'],
-        ['A', 'F']
-    ], {
-        'A': 10,
-        'B': 10,
-        'C': 10,
-        'D': 10,
-        'E': 10,
-        'F': 10
-    },
-        'C', ['A']) == 50, "Third"
-
-    print('Done. Try to check now. There are a lot of other tests')
+    disconnected_users([["A", "B"], ["B", "C"], ["C", "D"], ["C", "G"], ["G", "K"], ["K", "C"], ["A", "D"]],
+                       {"A": 10, "C": 10, "B": 10, "G": 10, "D": 10, "K": 10}, "D", ["B", "G"])
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    disconnected_users([["A", "B"], ["B", "C"], ["G", "D"], ["C", "G"], ["G", "K"], ["K", "C"]],
+                       {"A": 10, "C": 10, "B": 10, "G": 10, "D": 10, "K": 10}, "D", ["B", "G"])
